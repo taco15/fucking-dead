7d11fbd0052c6c6e9f0c1f3960e4e7559d924fdb
taco15 <59888768+taco15@users.noreply.github.com>
2020-12-21 22:18:33 +0100

    Update main.cpp

--- file menager/Src/main.cpp
+++ file menager/Src/main.cpp
@@ -1,2 +1,17 @@
 // i just started working on it
 // i think to 27.12.2020 i had pre alpha :)
+
+#include <iostream>
+#include "functions.hpp"
+
+using namespace std;
+using namespace FunctionClass;
+
+func F1;
+
+int main()
+{
+    
+}
+
+//made by Bruno Błeszyński
