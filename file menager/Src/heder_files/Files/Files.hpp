5685a88c68999b47ca4cd833c593c729e114f620
taco15 <59888768+taco15@users.noreply.github.com>
2020-12-21 23:14:03 +0100

    Add files via upload

---
+++ file menager/Src/heder_files/Files/Files.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+namespace FilesClass
+{
+    class Files
+    {
+        public:
+
+            // actions on files
+            void Ofiles(); // open files
+            void Cfile(); // copy files
+            void Pfile(); // past files
+            void Afile(); // add files
+            void Dfile(); // delete files
+            void Rfiles(); // rename files
+    };
+}
+
+
+//made by Bruno Błeszyński
---
+++ file menager/Src/heder_files/Folders/Folders.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+namespace FoldersClass
+{
+    class Folders
+    {
+        public:
+
+            // actions on folders
+            void Afolders(); // add folders
+            void Cfolders(); // copy folders
+            void Pfolders(); // past folders
+            void Afolders(); // add folders
+            void Dfolders(); // delete folders 
+            void Rfolders(); // rename folders
+    };
+}
+
+
+//made by Bruno Błeszyński
