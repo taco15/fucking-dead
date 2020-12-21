da9eba9e67e76495fe779f323b897d7463ed62da
taco15 <59888768+taco15@users.noreply.github.com>
2020-12-21 23:13:09 +0100

    Update main.cpp

--- file menager/Src/main.cpp
+++ file menager/Src/main.cpp
@@ -1,17 +1,31 @@
 // i just started working on it
-// i think to 27.12.2020 i had pre alpha :)
-
+// i think to 27.12.2020 i had pre alpha (meaby)
+ 
+// yo know #include
 #include <iostream>
-#include "functions.hpp"
+#include ".\src\heder_files\Files\Files.hpp"
+#include ".\src\heder_files\Folders\Folders.hpp"
 
+// jep exist other namespaces than std, better you can creat it
 using namespace std;
-using namespace FunctionClass;
+using namespace FilesClass;
+using namespace FoldersClass;
 
-func F1;
+// i dont have better names for this
+Files Fi;
+Folders Fo;
 
-int main()
+string UserText;
+
+int main() // main, bruh how do you don't know what is this
 {
-    
+
 }
+
+void searching() // searching enything on your comp disc
+{
+
+}
+
 
 //made by Bruno Błeszyński
